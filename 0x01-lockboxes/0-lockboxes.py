b8d9059607c1920c2ccd1c2865956e494e84013c
Simo Khanyi <simokhanyi@gmail.com>
2024-05-02 17:11:23 -0700

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,29 +6,21 @@ Lockboxes function
 
 def canUnlockAll(boxes):
     """
-    Function to check if all boxes can be unlocked.
+    Determines whether all locked boxes can be opened based on the keys
+    available.
     """
-    if type(boxes) is not list or len(boxes) == 0:
+    if not isinstance(boxes, list) or len(boxes) == 0:
         return False
 
     num_boxes = len(boxes)
-    unlocked = {0}  # Start with the first box unlocked
 
-    # Iterate over a copy of the set to avoid modifying it during iteration
-    for key in unlocked.copy():
-        for box in boxes[key]:
-            if 0 <= box < num_boxes and box not in unlocked:
-                unlocked.add(box)
+    for key in range(1, num_boxes):
+        box_checked = False
+        for idx in range(num_boxes):
+            if key in boxes[idx] and key != idx:
+                box_checked = True
+                break
+        if not box_checked:
+            return False
 
-    return len(unlocked) == num_boxes
-
-
-# Test cases
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # Output: True
-
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # Output: True
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # Output: False
+    return True
