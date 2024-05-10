769b6312db21c233ae333f1e4e4f497784fa299e
Simo Khanyi <simokhanyi@gmail.com>
2024-05-09 18:03:13 -0700

    Minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,29 +4,19 @@ minimum operations
 """
 
 
-def minOperations(n):
-    if n <= 1:
-        return n
-
-    # Initialize an array to store the minimum number of operations needed
-    dp = [0] * (n + 1)
-
-    for i in range(2, n + 1):
-        dp[i] = float('inf')  # Initialize to infinity
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n] if dp[n] != float('inf') else 0
-
-
-# Test cases
-n = 4
-print(
-    "Min number of operations to reach {} char: {}".format(n, minOperations(n))
-)
-
-n = 12
-print(
-    "Min number of operations to reach {} char: {}".format(n, minOperations(n))
-)
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while len(body) < n:
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
